f25557caa798950900a45f642cd0726f91497096
Yuta ISOGAI <isogai@en-corporation.biz>
2015-05-30 19:51:01 +0900

    TimelineViewController を作成

---
+++ SimpleTwitterClient/TimelineViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TimelineViewController.swift
+//  SimpleTwitterClient
+//
+//  Created by laishin on 2015/05/30.
+//  Copyright (c) 2015年 Re.Ra.Ku co., ltd. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
