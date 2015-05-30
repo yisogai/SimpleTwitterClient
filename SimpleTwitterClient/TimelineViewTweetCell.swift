dbd6d57fab360b5a45d37a3b413db754ebc37b69
Yuta ISOGAI <isogai@en-corporation.biz>
2015-05-30 21:26:50 +0900

    ツイート表示用 Cell のカスタムクラスを作成

---
+++ SimpleTwitterClient/TimelineViewTweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TimelineViewTweetCell.swift
+//  SimpleTwitterClient
+//
+//  Created by laishin on 2015/05/30.
+//  Copyright (c) 2015年 Re.Ra.Ku co., ltd. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewTweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
